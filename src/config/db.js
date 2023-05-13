61fd1242c708141552474c04d6acbf8ee8938a41
randrews <randrews@reciprocityindustries.com>
2023-05-12 21:51:37 -0600

    conn commit

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,11 @@ const { MONGODB_URI } = process.env;
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI, {
+        const conn = await mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         });
-        console.log("Connected to MongoDB");
+        console.log(`Connected to MongoDB: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
     }
