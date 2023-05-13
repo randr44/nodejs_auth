ca9ac876bcbe875667f3fd7766182663558318a8
randrews <randrews@reciprocityindustries.com>
2023-05-13 16:50:45 -0600

    adding dob to user

--- src/domains/user/model.js
+++ src/domains/user/model.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
     name: String,
     email: {type: String, unique: true},
     password: String,
+    dateOfBirth: Date,
     token: String,
     verified: {type: Boolean, default: false},
 });
